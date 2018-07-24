791d04a397a3b80e1ad71cb818c7cc10708eb3f3
Razwan Rashidi <razwan.izmi@gmail.com>
2018-07-25 01:39:01 +0800

    Build stateful component

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,28 @@ class Index extends React.Component {
     }));
   };
 
+  decrement = () => {
+    if (this.state.count === 0) return;
+
+    this.setState(({ count }) => ({
+      count: count - 1
+    }));
+  };
+
   render() {
     return (
       <Layout>
-        <h1>{this.state.count}</h1>
-        <button className="btn btn-primary" onClick={this.increment}>
-          Add
-        </button>
+        <div className="center-wrapper">
+          <h1>{this.state.count}</h1>
+          <div>
+            <button className="btn btn-danger mr-3" onClick={this.decrement}>
+              <i className="fas fa-minus" />
+            </button>
+            <button className="btn btn-success" onClick={this.increment}>
+              <i className="fas fa-plus" />
+            </button>
+          </div>
+        </div>
       </Layout>
     );
   }
