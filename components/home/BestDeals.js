e8b38a8148c4c238aec9d4fb74409bdff295a6e8
Razwan Rashidi <razwan.izmi@gmail.com>
2018-07-28 15:45:48 +0800

    Sorted arrows

--- components/home/BestDeals.js
+++ components/home/BestDeals.js
@@ -1,3 +1,4 @@
+import Slider from "react-slick";
 import "./best-deals.scss";
 
 const BestDeals = () => (
@@ -8,14 +9,43 @@ const BestDeals = () => (
     <div className="titled-box">
       <div className="titled-box__title">
         <span>
-          <i class="fas fa-minus"/> PROMOTIONS
+          <i class="fas fa-minus" /> PROMOTIONS
         </span>
       </div>
       <div className="titled-box__content">
-        hihi
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
       </div>
     </div>
   </section>
-)
+);
+
+const settings = {
+  slidesToShow: 2.3,
+  slidesToScroll: 1,
+  arrows: true,
+  // nextArrow: '<button type="button" class="slick-next icon fa-arrow-right"></button>',
+  // prevArrow: '<button type="button" class="slick-prev icon fa-arrow-left"></button>',
+  dots: true,
+  infinite: false
+};
 
 export default BestDeals;
